d6f66c9586ef3a7e545b2e0683657eadd64cb299
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-07-03 09:34:41 -0400

    v4.3.3

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ plot_interval = 10
 
 plot_gradient_intensity = 0.5
 num_samples_gradient = 200
-print('in config', plot_gradient_intensity)
 
 users = {
     "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['STAR_AMS', 'CL2','CL3','CL4','CL5','CL6','CL7']},
