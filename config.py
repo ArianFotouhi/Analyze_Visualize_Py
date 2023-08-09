a5923c8b3475aeddd01c5341d67a895c93e8fbc9
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-08-09 11:18:29 -0400

    v4.8.5

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-Date_col ='Date_Time'
-CLName_Col = 'CLIENT_NAME'
+Date_col ='UTC_DATE_TIME'
+CL_ID_Col = 'CLIENT_ID'
+CL_Name_Col = 'CLIENT_NAME'
 Lounge_ID_Col = 'Lounge_name'
 Volume_ID_Col = 'PAX_Accept'
 Airport_Name_Col = 'Airport_Code'
@@ -7,8 +8,8 @@ Country_Name_Col = 'Country'
 City_Name_Col = 'City'
 Refuse_Col='PAX_Reject'
 Ratio_Col='REF2ALW'
-
-
+Longitude_Col='Longitude'
+Latitude_Col = 'Latitude'
 
 #update them in index.html too
 time_alert = 30
@@ -19,14 +20,14 @@ plot_gradient_intensity = 0.5
 num_samples_gradient = 200
 
 users = {
-    "IEG": {"password": "pass", "ClientID": 'IEG', 'AccessCL':['STR', 'AC','AV','EK1','LH','LX','UA1']},
+    "IEG": {"password": "pass", "ClientID": 'IEG', 'AccessCL':['STR', 'AC','AV','EK1','LH','LX','UA1', 1]},
     "STR": {"password": "pass", "ClientID": 'STR', 'AccessCL':['STR', 'AC','AV','EK1','LH']},
     "AC": {"password": "pass", "ClientID": 'AC', 'AccessCL':['AC']},
     "LH": {"password": "pass", "ClientID": 'LH', 'AccessCL':['LH','LX']},
 }
 
 # Date_col ='DATE_UTC'
-# CLName_Col = 'CLIENT_NAME'
+# CL_ID_Col = 'CLIENT_NAME'
 # Lounge_ID_Col = 'lounge_name'
 # Volume_ID_Col = 'COUNT_PAX_ALLOWED'
 # Refuse_Col='COUNT_PAX_REFUSED'
